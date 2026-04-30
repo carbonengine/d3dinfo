8c095371b5f4f2116025ae6dd1035b105cbc298d
auddiolafs <32578333+auddiolafs@users.noreply.github.com>
2026-04-30 15:19:29 +0000

    correct blue find_dependency call

--- carbon-d3dinfoConfig.cmake
+++ carbon-d3dinfoConfig.cmake
@@ -1,4 +1,4 @@
 include(CMakeFindDependencyMacro)
 include(${CMAKE_CURRENT_LIST_DIR}/carbon-d3dinfo.cmake)
 
-find_dependency(Blue CONFIG NO_CMAKE_PATH REQUIRED)
+find_dependency(carbon-blue CONFIG REQUIRED)
